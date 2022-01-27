93c61ba65ef65742ad67ceed0fec932a29fdf423
Azim <zasynovazim@gmail.com>
2022-01-28 01:28:00 +0600

    Time update

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-document.body.innerHTML = '<div><p>Today is : Friday.</p><p>Current time is : 4 PM : 50 : 22</p></div>'
+function clock(){
+    let date = new Date()
+           year = date.getFullYear()
+           month = date.getMonth(2)
+           dates = date.getDate()
+           hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
+           minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
+           seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    document.getElementById('clock').innerHTML = year + "-" + month + "-" + dates + " - " + hours + ':' + minutes + ':' + seconds;
+  }
+  setInterval(clock, 1000);
+  clock();
+
+
