84047fa0ec73619e3e5fb98ba3863c0c44aadee1
Azim <zasynovazim@gmail.com>
2022-01-31 14:55:44 +0600

    add open.js

---
+++ my-app-react-five-task/component/open.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class TodoList extends React.Component{
+    render(){
+        return(
+            
+
+            <ol id="todo-list"> dskghsehg</ol>
+        )
+    }
+}
